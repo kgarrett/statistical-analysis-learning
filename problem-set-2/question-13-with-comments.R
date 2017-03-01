6a79463e6e92814fd2156cf0c77d72a447e2c1fc
Kelly Garrett <kelly.garrett@utexas.edu>
2017-03-01 08:58:54 -0600

    Create question-13-with-comments.R

---
+++ problem-set-2/question-13-with-comments.R
@@ -0,0 +1,9 @@
+#In this exercise you will create some simulated data and will fit simple linear regression models to it. Make sure to use set.seed(1) prior to starting part (a) to ensure consistent results.
+
+#a) Using the rnorm() function, create a vector, x, containing 100 observations drawn from a N(0, 1) distribution. This represents a feature, X.
+
+#b) Using the rnorm() function, create a vector, eps, containing 100 observations drawn from a N(0, 0.25) distribution i.e. a normal distribution with mean zero and variance 0.25.
+
+#c) Using x and eps, generate a vector y according to the model Y = −1+0.5X + . What is the length of the vector y? What are the values of β0 and β1 in this linear model?
+
+#d)
