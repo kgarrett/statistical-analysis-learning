a644b5b16a737453bda2b76220eede24461bd8b2
Kelly Garrett <kelly.garrett@utexas.edu>
2017-03-01 18:05:02 -0600

    Update question-13-with-comments.R

--- problem-set-2/question-13-with-comments.R
+++ problem-set-2/question-13-with-comments.R
@@ -6,4 +6,16 @@
 
 #c) Using x and eps, generate a vector y according to the model Y = −1+0.5X + . What is the length of the vector y? What are the values of β0 and β1 in this linear model?
 
-#d)
+#d) Create a scatterplot displaying the relationship between x and y. Comment on what you observe.
+
+#e) Fit a least squares linear model to predict y using x. Comment on the model obtained. How do βˆ0 and βˆ1 compare to β0 and β1?
+
+#f) Display the least squares line on the scatterplot obtained in (d). Draw the population regression line on the plot, in a different color. Use the legend() command to create an appropriate legend.
+
+#g) Now fit a polynomial regression model that predicts y using x and x2. Is there evidence that the quadratic term improves the model fit? Explain your answer.
+
+#h)  Repeat (a)–(f) after modifying the data generation process in such a way that there is less noise in the data. The model (3.39) should remain the same. You can do this by decreasing the variance of the normal distribution used to generate the error term in (b). Describe your results.
+
+#i) Repeat (a)–(f) after modifying the data generation process in such a way that there is more noise in the data. The model (3.39) should remain the same. You can do this by increasing the variance of the normal distribution used to generate the error term  in (b). Describe your results.
+
+#j) What are the confidence intervals for β0 and β1 based on the original data set, the noisier data set, and the less noisy data set? Comment on your results.
